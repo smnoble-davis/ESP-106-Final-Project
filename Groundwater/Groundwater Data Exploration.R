d8d37c10460b75df6cd6899372c3898bdf74f86f
smnoble95 <smnoble@ucdavis.edu>
2022-02-24 08:41:41 -0800

    more data exploration
    wells grouped to find min, max, and std
    
    trying to eliminate outliers for each well

--- Groundwater/Groundwater Data Exploration.R
+++ Groundwater/Groundwater Data Exploration.R
@@ -2,8 +2,85 @@ library(ggplot2)
 library(dplyr)
 setwd("/Users/shaelanoble/Documents/GitHub/ESP-106-Final-Project/Groundwater")
 kern_casgem <- read.csv("Kern_CASGEM_WellElevationData.csv")
+kern_casgem$Date <- as.Date(kern_casgem$Date, format = "%m/%d/%Y")
+kern_casgem$RPtoWS <- kern_casgem$RPtoWS*(-1)
+outliers <- kern_casgem[which(kern_casgem$RPtoWS>0,),]
+## changing positive values to negative values
+kern_casgem["365", 9] = -285.7
+kern_casgem["1621", 9] = -1
+kern_casgem["2026", 9] = -301.9
+kern_casgem["2279", 9] = -104.7
+## plot all measurements
+a <- ggplot(kern_casgem, aes(x=Date, y=RPtoWS, col=MasterSiteCode,group=MasterSiteCode)) + geom_point() + theme(legend.position = "none") + geom_line(aes(x=Date, y=RPtoWS))
+a
+## lat/long coordinates
+well_locations <- read.csv("well_locations.csv")
+## merge
+colnames(well_locations)[2] <- 'MasterSiteCode'
+kern_casgem_loc <- merge(kern_casgem, well_locations, by = "MasterSiteCode")
+## trying to do spatial stuff
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(tigris)
+CA_counties <- counties(state = "California", cb=TRUE)
+b <- ggplot()
+b <- b + geom_sf(data = CA_counties, color="black", fill="white", size=0.25)
+b
+## plot only kern county shapefile
+kern_county <- subset(CA_counties, NAME == "Kern")
+c <- ggplot()
+c <- c + geom_sf(data = kern_county, color="black", fill="white", size=0.25)
+c
+## convert casgem data to sf object?
+df <- st_as_sf(x = kern_casgem_loc, coords = c("LONGITUDE", "LATITUDE"))
+st_crs(df) <- st_crs(kern_county)
+## plot points on shapefile
+c <- ggplot()
+c <- c + geom_sf(data = kern_county, color="black", fill="white", size=0.25) + geom_sf(data = df)
+c
+##reference height for each well
+max_height <- aggregate(RPtoWS ~ MasterSiteCode, kern_casgem, function(x) max(x))
+
+colnames(max_height) <- c("MasterSiteCode", "m")
+df2 <- merge(kern_casgem, max_height, by= "MasterSiteCode")
+
+df2 <- df2 %>% 
+  group_by(MasterSiteCode) %>%
+  mutate(depth_std = RPtoWS - m) %>%
+  ungroup()
+
+##wells plotted by reference height
+d <- ggplot(df2, aes(x=Date, y=depth_std, col=MasterSiteCode,group=MasterSiteCode)) + geom_point() + theme(legend.position = "none") + geom_line(aes(x=Date, y=depth_std))
+d
+
+##trying to find outliers in individual wells
+df3 <- kern_casgem %>% 
+  group_by(MasterSiteCode) %>%
+  summarize(sd = sd(RPtoWS, na.rm=TRUE), mean = mean(RPtoWS, na.rm=TRUE), min = min(RPtoWS, na.rm=TRUE), max = max(RPtoWS, na.rm=TRUE), n=n()) %>%
+  ungroup()
+
+##plotting individual wells
+e <- ggplot(subset(kern_casgem, MasterSiteCode=="356684N1193954W002"), aes(x=Date, y=RPtoWS, col=MasterSiteCode,group=MasterSiteCode)) + geom_point() + theme(legend.position = "none") + geom_line(aes(x=Date, y=RPtoWS))
+e
+
+data_check <- subset(kern_casgem, MasterSiteCode=="356684N1193954W002")
+
+
+select
+filter
+summarize
+arrange
+
+library(data.table)
+kern_casgem_dt <- as.data.table(kern_casgem)
+maxvalues <- kern_casgem_dt[kern_casgem_dt[, .I[RPtoWS== max(RPtoWS)], by=MasterSiteCode]$V1]
+
+max(kern_casgem$MasterSiteCode==344779N1192479W001)
+
+
 th <- theme_bw(legend.position="none")
-a <- ggplot(kern_casgem, aes(x=Date, y=RPtoWS, col=MasterSiteCode)) + geom_line() + theme(legend.position = "none") + ylim(0, 300)
+a <- ggplot(kern_casgem, aes(x=Date, y=RPtoWS, col=MasterSiteCode)) + geom_line() + theme(legend.position = "none") 
 a
 test <- subset(kern_casgem, MasterSiteCode == "344779N1192479W001")
 test$RPtoWS <- test$RPtoWS*(-1)
