d75cbff081c31b3c0f812bf68e3d4d124d4bc341
steenbean <97637560+steenbean@users.noreply.github.com>
2022-02-28 15:01:03 -0800

    Update Ag_ExploreDat.R

--- Ag Production/Ag_ExploreDat.R
+++ Ag Production/Ag_ExploreDat.R
@@ -1,21 +1,15 @@
 #read in ag crop production data files 2009-2020
 library(tidyverse)
 library("stringr")
-library("ggplot2")
-install.packages("purrr")
-library("purrr")
-install.packages("dplyr")
-library("dplyr")
+
+#Set Directory so that if you gave one folder with all your data, someone could read in each data set by only chaning the set directory at the top.
+
+#For all data sets for years 2009-2020, write subset function so we only see data for the 5 counties of interest: Fresno, Kern, Monterey, Merced and Tulare.
+#Remove crops that aren't produce or feed (e.g. hay) by finding all crops that contain the words: apiary, cattle, fish, hogs, chickens, eggs, livestock, manure, milk, poultry, sheep, turkeys, wool, flowers, forest products, nursery products, pasture, silage, ostrich,birds,goats (str_detect from stringr package)
+#Notice that some data sets have a space at the end of the County name and crop names, while some data sets do not. 
 
 #2009 DATA
 ag2009=read.csv("/Users/christinaharrington/Desktop/ESP-106-Final-Project/Ag Production/2009cropyear.csv")
-
-#see how there's a "state totals" and "sum of others" rows at the end of every crop section.
-#notice units are in tons or US$
-#write subset function to remove rows that have value 'sum of others' and 'state totals' in the County column
-#Remove non-crops: apiary, cattle, fish, hogs, chickens, eggs, livestock, manure, milk, poultry, sheep, turkeys, wool, flowers, forest products, nursery products, pasture, silage, ostrich,birds,goats (str_detect from stringr package)
-#sum total $ production for all crops
-#Be sure to remove 'na' values when summing/plotting - unless situation where you'd want to show? 
 
 ag2009_cntcrops=subset(ag2009,County%in%c("Fresno ","Kern ","Monterey ","Merced ","Tulare ") &
                       FALSE == str_detect(Crop.Name,"APIARY") &
@@ -41,9 +35,11 @@ ag2009_cntcrops=subset(ag2009,County%in%c("Fresno ","Kern ","Monterey ","Merced 
                         FALSE == str_detect(Crop.Name,"GOATS") &
                         FALSE == str_detect(Crop.Name,"OSTRICH") &
                         FALSE == str_detect(Crop.Name,"BIOMASS "))
-
-ggplot(ag2009_cntcrops,aes(County,Value))+
-  geom_bar(stat="sum")
+ag2009_cntcrops$County[ag2009_cntcrops$County == "Kern "]="Kern"
+ag2009_cntcrops$County[ag2009_cntcrops$County == "Fresno "]="Fresno"
+ag2009_cntcrops$County[ag2009_cntcrops$County == "Tulare "]="Tulare"
+ag2009_cntcrops$County[ag2009_cntcrops$County == "Monterey "]="Monterey"
+ag2009_cntcrops$County[ag2009_cntcrops$County == "Merced "]="Merced"
 
 
 #2010 DATA
@@ -72,9 +68,15 @@ ag2010_cntcrops=subset(ag2010,County%in%c("Fresno ","Kern ","Monterey ","Merced 
                          FALSE == str_detect(Crop.Name,"GOATS") &
                          FALSE == str_detect(Crop.Name,"OSTRICH") &
                          FALSE == str_detect(Crop.Name,"BIOMASS "))
+ag2010_cntcrops$County[ag2010_cntcrops$County == "Kern "]="Kern"
+ag2010_cntcrops$County[ag2010_cntcrops$County == "Fresno "]="Fresno"
+ag2010_cntcrops$County[ag2010_cntcrops$County == "Tulare "]="Tulare"
+ag2010_cntcrops$County[ag2010_cntcrops$County == "Monterey "]="Monterey"
+ag2010_cntcrops$County[ag2010_cntcrops$County == "Merced "]="Merced"
+
+
 
 #2011 DATA
-
 ag2011=read.csv("/Users/christinaharrington/Desktop/ESP-106-Final-Project/Ag Production/2011cropyear.csv")
 ag2011_cntcrops=subset(ag2011,County%in%c("Fresno ","Kern ","Monterey ","Merced ","Tulare ") &
                          FALSE == str_detect(Crop.Name,"APIARY") &
@@ -100,6 +102,12 @@ ag2011_cntcrops=subset(ag2011,County%in%c("Fresno ","Kern ","Monterey ","Merced 
                          FALSE == str_detect(Crop.Name,"GOATS") &
                          FALSE == str_detect(Crop.Name,"OSTRICH") &
                          FALSE == str_detect(Crop.Name,"BIOMASS "))
+ag2011_cntcrops$County[ag2011_cntcrops$County == "Kern "]="Kern"
+ag2011_cntcrops$County[ag2011_cntcrops$County == "Fresno "]="Fresno"
+ag2011_cntcrops$County[ag2011_cntcrops$County == "Tulare "]="Tulare"
+ag2011_cntcrops$County[ag2011_cntcrops$County == "Monterey "]="Monterey"
+ag2011_cntcrops$County[ag2011_cntcrops$County == "Merced "]="Merced"
+
 
 #2012 DATA
 ag2012=read.csv("/Users/christinaharrington/Desktop/ESP-106-Final-Project/Ag Production/2012cropyear.csv")
@@ -127,7 +135,13 @@ ag2012_cntcrops=subset(ag2012,County%in%c("Fresno ","Kern ","Monterey ","Merced 
                          FALSE == str_detect(Crop.Name,"GOATS") &
                          FALSE == str_detect(Crop.Name,"OSTRICH") &
                          FALSE == str_detect(Crop.Name,"BIOMASS "))
-                         
+ag2012_cntcrops$County[ag2012_cntcrops$County == "Kern "]="Kern"
+ag2012_cntcrops$County[ag2012_cntcrops$County == "Fresno "]="Fresno"
+ag2012_cntcrops$County[ag2012_cntcrops$County == "Tulare "]="Tulare"
+ag2012_cntcrops$County[ag2012_cntcrops$County == "Monterey "]="Monterey"
+ag2012_cntcrops$County[ag2012_cntcrops$County == "Merced "]="Merced"         
+
+              
 
 #2013 DATA
 ag2013=read.csv("/Users/christinaharrington/Desktop/ESP-106-Final-Project/Ag Production/2013cropyear.csv")
@@ -261,6 +275,13 @@ ag2017_cntcrops=subset(ag2017,County%in%c("Fresno ","Kern ","Monterey ","Merced 
                          FALSE == str_detect(Crop.Name,"GOATS ") &
                          FALSE == str_detect(Crop.Name,"OSTRICH ") &
                          FALSE == str_detect(Crop.Name,"BIOMASS "))
+ag2017_cntcrops$County[ag2017_cntcrops$County == "Kern "]="Kern"
+ag2017_cntcrops$County[ag2017_cntcrops$County == "Fresno "]="Fresno"
+ag2017_cntcrops$County[ag2017_cntcrops$County == "Tulare "]="Tulare"
+ag2017_cntcrops$County[ag2017_cntcrops$County == "Monterey "]="Monterey"
+ag2017_cntcrops$County[ag2017_cntcrops$County == "Merced "]="Merced"
+
+
 #2018 DATA
 ag2018err=read.csv("/Users/christinaharrington/Desktop/ESP-106-Final-Project/Ag Production/2018cactbsErrata.csv")
 ag2018_cntcrops=subset(ag2018err,County%in%c("Fresno ","Kern ","Monterey ","Merced ","Tulare ") &
@@ -288,6 +309,14 @@ ag2018_cntcrops=subset(ag2018err,County%in%c("Fresno ","Kern ","Monterey ","Merc
                          FALSE == str_detect(Crop.Name,"OSTRICH ") &
                          FALSE == str_detect(Crop.Name,"BIOMASS "))
 
+ag2018_cntcrops$County[ag2018_cntcrops$County == "Kern "]="Kern"
+ag2018_cntcrops$County[ag2018_cntcrops$County == "Fresno "]="Fresno"
+ag2018_cntcrops$County[ag2018_cntcrops$County == "Tulare "]="Tulare"
+ag2018_cntcrops$County[ag2018_cntcrops$County == "Monterey "]="Monterey"
+ag2018_cntcrops$County[ag2018_cntcrops$County == "Merced "]="Merced"
+
+
+
 #2019 DATA
 ag2019=read.csv("/Users/christinaharrington/Desktop/ESP-106-Final-Project/Ag Production/2019cropyear.csv")
 ag2019_cntcrops=subset(ag2019,County%in%c("Fresno ","Kern ","Monterey ","Merced ","Tulare ") &
@@ -314,6 +343,13 @@ ag2019_cntcrops=subset(ag2019,County%in%c("Fresno ","Kern ","Monterey ","Merced 
                          FALSE == str_detect(Crop.Name,"GOATS ") &
                          FALSE == str_detect(Crop.Name,"OSTRICH ") &
                          FALSE == str_detect(Crop.Name,"BIOMASS "))
+ag2019_cntcrops$County[ag2019_cntcrops$County == "Kern "]="Kern"
+ag2019_cntcrops$County[ag2019_cntcrops$County == "Fresno "]="Fresno"
+ag2019_cntcrops$County[ag2019_cntcrops$County == "Tulare "]="Tulare"
+ag2019_cntcrops$County[ag2019_cntcrops$County == "Monterey "]="Monterey"
+ag2019_cntcrops$County[ag2019_cntcrops$County == "Merced "]="Merced"
+
+
 #2020 DATA
 ag2020=read.csv("/Users/christinaharrington/Desktop/ESP-106-Final-Project/Ag Production/2020cropyear.csv")
 ag2020_cntcrops=subset(ag2020,County%in%c("Fresno ","Kern ","Monterey ","Merced ","Tulare ") &
@@ -340,33 +376,57 @@ ag2020_cntcrops=subset(ag2020,County%in%c("Fresno ","Kern ","Monterey ","Merced 
                          FALSE == str_detect(Crop.Name,"GOATS ") &
                          FALSE == str_detect(Crop.Name,"OSTRICH ") &
                          FALSE == str_detect(Crop.Name,"BIOMASS "))
+ag2020_cntcrops$County[ag2020_cntcrops$County == "Kern "]="Kern"
+ag2020_cntcrops$County[ag2020_cntcrops$County == "Fresno "]="Fresno"
+ag2020_cntcrops$County[ag2020_cntcrops$County == "Tulare "]="Tulare"
+ag2020_cntcrops$County[ag2020_cntcrops$County == "Monterey "]="Monterey"
+ag2020_cntcrops$County[ag2020_cntcrops$County == "Merced "]="Merced"
+
+#Change the 2020 data set column names so it matches the column names of the other data sets
 ag2020_cntcrops=rename(ag2020_cntcrops,Yield=Yield..Unit.Acre.,Price.P.U=Price..Dollars.Unit.,Value=Value..Dollars.)
 
 
-#Now combine all data frames.Some years have differing numbers of observations, so you'll want to use the binc_row function.
-
+#Now combine all data frames so you see all counties for every year 2009-2020.Some years have differing numbers of observations, so you'll want to use the bind_row function.
 
 allag=bind_rows(ag2009_cntcrops,ag2010_cntcrops,ag2011_cntcrops,ag2011_cntcrops,ag2012_cntcrops,ag2013_cntcrops,ag2014_cntcrops,ag2015_cntcrops,ag2016_cntcrops,ag2017_cntcrops,ag2018_cntcrops,ag2019_cntcrops,ag2020_cntcrops)
 
-#Create some exploratory data plots & stats
+
+#Create some exploratory data plots & stats of the new data frame
 
 yield_summary=print(summary(allag$Yield))
 prod_summary=print(summary(allag$Production))
 value_summary=print(summary(allag$Value))
 harvest_summary=print(summary(allag$Harvested.Acres))
 
-#Create data frame that has columns Year, County, Total Value. Need to sum the value of all crops for each county. 
-#What is the sum of all value in the county of Fresno for each year (2009-2020)?
-#use 'group_by' function? How to group by County and Year?
-#NEED TO RENAME VARIABLES TO REMOVE SPACE E.G. COUNTY NAME
+#We're interested in the total value of production for each county each year. Create data frame that has columns Year, County, Total Value only. Need to sum the value of all crops for each county per year. 
+#Use 'group_by' and 'summarize' functions in dplyr
 
-newdataframe <- allag %>%
-  group_by(County&Year) %>%
-  summarize(sum = sum(Value))
+county_yr_total=allag %>%
+  group_by(County,Year) %>%
+  summarize(Value_sum = sum(Value))
 
-group_by(Year) %>%
+#Produce exploratory data for new dataframe
+YrCounty_Valuesummary=print(summary(county_yr_total$Value_sum))
 
+YrCountyBoxplot=print(
+  ggplot(county_yr_total,aes(County,Value_sum))+
+  geom_boxplot()+
+  ylab("Total Production Value from 2009-2020 (US$)"))
 
+#Per the boxplot, even though there are seemingly outliers in the data, we won't remove these values because we're not assuming this is due to miscalculation.
+#These varying values are likely due to changes in environmental conditions.
+
+#Plot the county production values over time
+YrCountyLine=print(
+  ggplot(county_yr_total,aes(Year,Value_sum,group=County,color=County))+
+  geom_line()+
+  scale_x_continuous(name="Production Year",breaks=c(2009:2020))+
+  scale_y_continuous(name="Total Production Value (US$)"))
+
+#NEED TO DO
+#Per the rubric, best if you're not hard coding file path names, so set Directory so that if you gave one folder with all your data, someone could read in each data set by only chaning the set directory at the top.
+#Double check County ag reports to see if the county production value is way off or not...
+#use "trimws" function to remove space after County and Crop Name to avoid double counting...
 
 
 
